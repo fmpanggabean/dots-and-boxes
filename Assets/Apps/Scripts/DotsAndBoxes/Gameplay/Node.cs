9f900d6f9b28a128eae84a256bfbfdb425da7f06
F.M. Panggabean <cichio.maru@gmail.com>
2022-07-15 15:22:20 +0700

    add line connection logic
    add line connection logic

--- Assets/Apps/Scripts/DotsAndBoxes/Gameplay/Node.cs
+++ Assets/Apps/Scripts/DotsAndBoxes/Gameplay/Node.cs
@@ -1,18 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class Node : MonoBehaviour
+namespace DotsAndBoxes.Gameplay
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Node : MonoBehaviour
     {
-        
-    }
+        public List<Node> connectionList;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private LineRenderer[] lineRenderer;
+        private int currentLine;
+
+        private void Awake() {
+            lineRenderer = GetComponentsInChildren<LineRenderer>();
+            currentLine = 0;
+        }
+
+        internal void BeginLineRenderer() {
+            lineRenderer[currentLine].SetPosition(0, GetPosition());
+        }
+
+        internal void DrawLineTo(Vector3 position) {
+            lineRenderer[currentLine].SetPosition(1, position);
+        }
+        internal void DrawLineTo(Node nodeEnd) {
+            lineRenderer[currentLine].SetPosition(1, nodeEnd.GetPosition());
+        }
+
+        internal bool IsAvailableToConnectTo(Node endNode) {
+            if (connectionList.Contains(endNode)) {
+                Debug.Log("Unable to connect. Start Node already contain End Node");
+                return false;
+            } 
+            if (endNode.connectionList.Contains(this)) {
+                Debug.Log("Unable to connect. End Node already contain Start Node");
+                return false;
+            }
+            return true;
+        }
+
+        internal void AddConnection(Node endNode) {
+            Debug.Log("Node Connected.");
+            connectionList.Add(endNode);
+            endNode.connectionList.Add(this);
+
+            MoveToNextLineRenderer();
+        }
+
+        private void MoveToNextLineRenderer() {
+            currentLine++;
+        }
+
+        internal void CancelLine() {
+            lineRenderer[currentLine].SetPosition(1, GetPosition());
+        }
+
+        private Vector3 GetPosition() {
+            return transform.position;
+        }
     }
 }
